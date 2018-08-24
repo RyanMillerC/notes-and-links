9387535a3535340d4af09b3d1b4a3fce285798d5
Ryan Miller <ryan@devopsmachine.com>
2018-08-24 14:42:09 -0400

    Add return MD file as list function to utils

--- test/utils/__init__.py
+++ test/utils/__init__.py
@@ -8,3 +8,11 @@ def read_file_to_string(file_name):
     with open(file_name, 'r') as stream:
         output = stream.read()
     return output
+
+
+def read_file_to_list(file_name):
+    """Read file from disk and return list with file contents.
+    """
+    with open(file_name, 'r') as stream:
+        output = stream.readlines()
+    return output
